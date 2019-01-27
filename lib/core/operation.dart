79da2d45aba964baf8e6d6111b621a08340d8dba
Fu Zhang <contact@fzhang.co.uk>
2019-01-27 05:39:05 +0000

    Added a text label and a button which do simple operation

---
+++ lib/core/operation.dart
@@ -0,0 +1,22 @@
+
+
+class Operation{
+ 
+ static plus(int operand, int operand2){
+   return operand+operand2;
+ }
+
+  static minus(int operand, int operand2){
+   return operand-operand2;
+ }
+
+  static mulit(int operand, int operand2){
+   return operand*operand2;
+ }
+
+   static devid(int operand, int operand2){
+   return operand/operand2;
+ }
+
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,67 @@
 import 'package:flutter/material.dart';
+
+import 'core/operation.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatefulWidget{
+  
+  @override
+  _MyAppState createState() => new _MyAppState();
+
+}
+
+
+
+class _MyAppState extends State<MyApp>{
+
+  String result = "0" ;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "Dart Calculator",
+      home:Scaffold(
+        appBar: AppBar(title: Text("Dart Calculator")),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(result,style: TextStyle(fontSize: 50.0),)
+                ],
+               )
+              ),
+          Expanded(
+           child: Column(
+             children: <Widget>[
+               Row(
+                 children: <Widget>[
+                   new RaisedButton(
+                     child: const Text("+"),
+                     onPressed:(){
+                        setState((){
+                           result = (Operation.plus(1, 5).toString());
+                       });
+                     }
+                   )
+                 ],
+               )
+             ],
+           ),
+         )
+          
+
+       ]
+     )
+    ), 
+   );
+  }
+
+
+}
+
